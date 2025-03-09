5ff36821c2f5c2c81323af8e6c14bb4c1c6c8459
triginarsa <adi@cakeplabs.com>
2025-03-09 21:45:50 +0800

    feat: validation

--- src/Textarea.tsx
+++ src/Textarea.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let newText = e.target.value;
+
+    //basic validation
+    if (newText.includes("<script>")) {
+      setWarningText("No script tags allowed!!");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("No @ allowed!!");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        placeholder="Enter text here..."
+        spellCheck="false"
+        value={text}
+        onChange={handleChange}
+      />
+      {warningText && <Warning warningText={warningText} />}
+    </div>
+  );
 }
