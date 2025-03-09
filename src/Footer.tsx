0d4052e8d431b6cf22f8d98206973640833236ca
triginarsa <adi@cakeplabs.com>
2025-03-09 16:01:54 +0800

    first commit

---
+++ src/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; copyright 2025 by Triginarsa</small>
+      <small>Last checked limits: 17 days ago</small>
+    </footer>
+  );
+}
